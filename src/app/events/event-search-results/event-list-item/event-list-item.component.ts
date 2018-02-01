0ab1d7cbabf8fb8e4da8dbdd65b0a18b8e97c3dc
elias-garcia <eliasgarciadev@gmail.com>
2018-02-01 11:22:49 +0100

    Event list item styling

--- src/app/events/event-search-results/event-list-item/event-list-item.component.ts
+++ src/app/events/event-search-results/event-list-item/event-list-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EventResponse } from '../../../shared/models/event.model';
+import { EventService } from '../../../core/services/event.service';
+import { GeolocationService } from '../../../core/services/geolocation.service';
+import { } from '@types/google-maps';
 
 @Component({
   selector: 'app-event-list-item',
@@ -10,9 +13,24 @@ export class EventListItemComponent implements OnInit {
 
   @Input() public event: EventResponse;
 
-  constructor() { }
+  public playersNumber: number;
+  public eventLocation: string;
+
+  constructor(
+    private eventService: EventService,
+    private geolocationService: GeolocationService
+  ) { }
 
   ngOnInit() {
+    this.eventService.getEventPlayers(this.event.id)
+      .map((res: any) => res.data.players.length)
+      .subscribe((playersNumber: number) => this.playersNumber = playersNumber);
+    this.geolocationService.reverseGeocode(this.event.location[0], this.event.location[1])
+      .subscribe((results: google.maps.GeocoderResult[]) => {
+        if (results && results.length) {
+          this.eventLocation = results[0].formatted_address;
+        }
+      });
   }
 
 }
