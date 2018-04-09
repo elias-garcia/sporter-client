11af0158df5aa0bb60a335d97ce94dac6c36807c
elias-garcia <eliasgarciadev@gmail.com>
2018-04-09 21:52:17 +0200

    Production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://sporter-api-env.eu-central-1.elasticbeanstalk.com:3000/api',
+  webSocketsUrl: 'http://sporter-api-env.eu-central-1.elasticbeanstalk.com:3000',
+  googlePlacesApiKey: 'AIzaSyAT5_oM7Uw3AcZgGT98TT-8MY8JIvRBtrI',
+  defaultPageSize: '5'
 };
