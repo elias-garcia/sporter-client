809be5f5c76a9135156ef987a33c5284d287f3b4
elias-garcia <eliasgarciadev@gmail.com>
2018-04-10 20:24:25 +0200

    Fixed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://sporter-api.herokuapp.com/api',
-  webSocketsUrl: 'https://sporter-api.herokuapp.com/',
+  webSocketsUrl: 'https://sporter-api.herokuapp.com',
   googlePlacesApiKey: 'AIzaSyAT5_oM7Uw3AcZgGT98TT-8MY8JIvRBtrI',
   defaultPageSize: '5'
 };
